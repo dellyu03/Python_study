dd5232487a6d6de3075293ac5c3c7219fb5eca29
dellyu22 <ysh00@g.hongik.ac.kr>
2022-12-01 14:58:12 +0900

    study

--- webscraping/_bs4.py
+++ webscraping/_bs4.py
@@ -2,18 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://comic.naver.com/webtoon/weekday"
+url = "https://comic.naver.com/webtoon/list?titleId=790713&weekday=fri"
 res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
+cartoons = soup.find_all("td", attrs = {"class" : "title"} )
 
+for cartoon in cartoons:
+    title = cartoon.a.get_text()
+    link = "https://comic.naver.com" + cartoon.a["href"]
+    print(title, link)
+
+'''
 print(soup.title)
 print(soup.title.get_text())
 
 print(soup.a)
 print(soup.a.attrs)
 print(soup.a["href"])
+'''
 
 '''
 print(soup.find("a", attrs={"class": "Nbtn_upload"}))
@@ -29,6 +37,7 @@ for otherRank in otherRanks:
     print("rank {}: {}".format(i, otherRank.a.get_text()))
 '''
 
+'''
 cartoons = soup.find_all("a", attrs = {"class" : "title"})
 print('만화 수 : ', len(cartoons))
 
@@ -36,4 +45,4 @@ for i, cartoon in enumerate(cartoons):
     if i == 15:
         break
     print(cartoon.get_text())
-
+'''
